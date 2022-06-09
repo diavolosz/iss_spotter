083b7832c0df19bd1a5930cb58b7cf7ce9a16415
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-09 22:35:12 +0000

    added flyover time of iss

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+
+
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, flyoverTime) => {
+
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover time:', flyoverTime);
+})
+
 
 // fetchCoordsByIP("99.241.100.180", (error, GeoCoor) => {
 //   if (error) {
--- iss.js
+++ iss.js
@@ -50,8 +50,49 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
+// { latitude: '49.27670', longitude: '-123.13000' }
+
+
+const fetchISSFlyOverTimes = function(geoCoor, callback) {
+  
+  let url = `https://iss-pass.herokuapp.com/json/?lat=${geoCoor.latitude}&lon=${geoCoor.longitude}`;
+
+  request(url, (error, response, body) => {
+
+    if (error) {
+      callback(error);
+      return;
+    }
+  
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    let data = JSON.parse(body).response;
+
+    for (let flyover of data) {
+      let flyoverTime = {};
+      flyoverTime.risetime = flyover.risetime;
+      flyoverTime.duration = flyover.duration;
+      callback(null, flyoverTime);
+    }
+  });
+};
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 };
