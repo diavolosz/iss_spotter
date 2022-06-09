717433e0b9e5622ec0c0f4de7a5a7209d23d0a05
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-09 22:13:07 +0000

    added added geo coor fetching mechanism

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP("99.241.100.180", (error, GeoCoor) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Geo coordiates:' , GeoCoor);
+// });
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
--- iss.js
+++ iss.js
@@ -24,4 +24,34 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+
+const fetchCoordsByIP = function(ip, callback) {
+
+  let url = `https://api.ipbase.com/v1/json/${ip}`;
+
+  request(url, (error, response, body) => {
+
+    if (error) {
+      callback(error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    let GeoCoor = {};
+    GeoCoor.latitude = JSON.parse(body).latitude;
+    GeoCoor.longitude = JSON.parse(body).longitude;
+    callback(null, GeoCoor);
+  });
+};
+
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
